87caa2fac1e50bdc00645b80214beb3ddf0871a1
Markéta Calábková <meggy.calabkova@gmail.com>
2020-02-11 14:35:36 +0100

    first attempt to refactor

--- rpmlint/checks/I18NCheck.py
+++ rpmlint/checks/I18NCheck.py
@@ -1,11 +1,3 @@
-#############################################################################
-# File          : I18NCheck.py
-# Package       : rpmlint
-# Author        : Frederic Lepied
-# Created on    : Mon Nov 22 20:02:56 1999
-# Purpose       : checks i18n bugs.
-#############################################################################
-
 import re
 
 import rpm
@@ -27,7 +19,7 @@ INCORRECT_LOCALES = {
     'en_UK': 'en_GB'}
 
 package_regex = re.compile('-(' + '|'.join(LANGUAGES) + ')$')
-locale_regex = re.compile('^(/usr/share/locale/([^/]+))/')
+locale_regex = re.compile('^(/usr/share/locale/([^/]+))')
 correct_subdir_regex = re.compile('^(([a-z][a-z]([a-z])?(_[A-Z][A-Z])?)([.@].*$)?)$')
 lc_messages_regex = re.compile('/usr/share/locale/([^/]+)/LC_MESSAGES/.*(mo|po)$')
 man_regex = re.compile('/usr(?:/share)?/man/([^/]+)/man[0-9n][^/]*/[^/]+$')
@@ -72,17 +64,14 @@ class I18NCheck(AbstractCheck):
     def check_binary(self, pkg):
         files = list(pkg.files().keys())
         files.sort()
-        locales = []                      # list of locales for this packages
+        locales_list = ()
         webapp = False
+            
+        incorrect_tags = ()
+        self._check_i18n_tags(pkg, incorrect_tags)
 
-        i18n_tags = pkg[rpm.RPMTAG_HEADERI18NTABLE] or ()
-
-        for i in i18n_tags:
-            try:
-                correct = INCORRECT_LOCALES[i]
-                self.output.add_info('E', pkg, 'incorrect-i18n-tag-' + correct, i)
-            except KeyError:
-                pass
+        for i in incorrect_tags:
+            self.output.add_info('E', pkg, 'incorrect-i18n-tag-' + INCORRECT_LOCALES[i], i)
 
         # as some webapps have their files under /var/www/html, and
         # others in /usr/share or /usr/lib, the only reliable way
@@ -96,54 +85,116 @@ class I18NCheck(AbstractCheck):
             res = locale_regex.search(f)
             if res:
                 locale = res.group(2)
-                # checks the same locale only once
-                if locale not in locales:
-                    locales.append(locale)
-                    res2 = correct_subdir_regex.search(locale)
-                    if not res2:
-                        if locale not in EXCEPTION_DIRS:
-                            self.output.add_info('E', pkg, 'incorrect-locale-subdir', f)
+
+                b, correct = self._check_unknown_locale(pkg, locale, locales_list)
+                if not b:
+                    if not correct:
+                        self.output.add_info('E', pkg, 'incorrect-locale-subdir', f)
                     else:
-                        locale_name = res2.group(2)
-                        try:
-                            correct = INCORRECT_LOCALES[locale_name]
-                            self.output.add_info('E', pkg, 'incorrect-locale-' + correct, f)
-                        except KeyError:
-                            pass
+                        self.output.add_info('E', pkg, 'incorrect-locale-' + correct, f)
+
             res = lc_messages_regex.search(f)
-            subdir = None
-            if res:
-                subdir = res.group(1)
-                if not is_valid_lang(subdir):
-                    self.output.add_info('E', pkg, 'invalid-lc-messages-dir', f)
-            else:
-                res = man_regex.search(f)
+
+            b, subdir = self._check_valid_dir(pkg, res)
+            if not b:
                 if res:
-                    subdir = res.group(1)
-                    if is_valid_lang(subdir):
-                        subdir = None
-                    else:
-                        self.output.add_info('E', pkg, 'invalid-locale-man-dir', f)
+                    self.output.add_info('E', pkg, 'invalid-lc-messages-dir', f)
+                else:
+                    self.output.add_info('E', pkg, 'invalid-locale-man-dir', f)
 
             if f.endswith('.mo') or subdir:
-                if pkg.files()[f].lang == '' and not webapp:
-                    self.output.add_info('W', pkg, 'file-not-in-%lang', f)
+            if pkg.files()[f].lang == '' and not webapp:
+                self.output.add_info('W', pkg, 'file-not-in-%lang', f)
 
+        not_in_lang = ()
+        self._check_lang_consistency(pkg, not_in_lang)
+        for f in not_in_lang:
+            self.output.add_info('E', pkg, 'subfile-not-in-%lang', f)
+
+        b, locales = self._check_dependency_on_locales(pkg):
+            if b:
+                self.output.add_info('E', pkg, 'no-dependency-on', locales)
+
+    def _check_dependency_on_locales(self, pkg):
+        """
+        Checks whether the package depends on correct locale package.
+        ??? Why? Isn't it obsolete?
+        """
+        name = pkg.name
+        res = package_regex.search(name)
+        if res:
+            locales = 'locales-' + res.group(1) # the '-' is not needed
+            if locales != name and locales not in (x[0] for x in pkg.requires()):
+                return False, locales
+            return True, locales
+        return True, None
+
+    def _check_i18n_tags(self, pkg, result_list):
+        """
+        Checks i18n tags in the header and marks the incorrect ones.
+        """
+        i18n_tags = pkg[rpm.RPMTAG_HEADERI18NTABLE] or ()
+        for i in i18n_tags:
+            try:
+                correct = INCORRECT_LOCALES[i]
+                result_list.append(i)
+            except KeyError:
+                pass
+
+    def _check_lang_consistency(self, pkg, result_list):
+        """
+        Check whether... ??? I don't exactly know.
+        """
+        files = list(pkg.files().keys())
         main_dir, main_lang = ('', '')
         for f in files:
             lang = pkg.files()[f].lang
             if main_lang and lang == '' and is_prefix(main_dir + '/', f):
-                self.output.add_info('E', pkg, 'subfile-not-in-%lang', f)
+                result_list.append(f)
             if main_lang != lang:
                 main_dir, main_lang = f, lang
 
-        name = pkg.name
-        res = package_regex.search(name)
+    def _check_unknown_locale(self, pkg, locale, locales):
+        """
+        Check whether we know the locale and is correct.
+        """
+        # checks the same locale only once
+        if locale not in locales:
+            locales.append(locale)
+            res2 = correct_subdir_regex.search(locale)
+            if not res2:
+                if locale not in EXCEPTION_DIRS:
+                    return False, None
+            else:
+                locale_name = res2.group(2)
+                try:
+                    correct = INCORRECT_LOCALES[locale_name]
+                    return False, correct
+                except KeyError:
+                    pass
+        return True, None
+
+    def _check_valid_dir(self, pkg, res):
+        """ 
+        Checks wheteher we install locales or localized man files to valid directories.
+
+        Little brother of _check_unknown_locale, in the meaning that the two regexes are similar.
+        """
+        subdir = None
         if res:
-            locales = 'locales-' + res.group(1)
-            if locales != name:
-                if locales not in (x[0] for x in pkg.requires()):
-                    self.output.add_info('E', pkg, 'no-dependency-on', locales)
+            subdir = res.group(1)
+            if not is_valid_lang(subdir):
+                return False, subdir
+        else:
+            res = man_regex.search(f)
+            if res:
+                subdir = res.group(1)
+                if is_valid_lang(subdir):
+                    subdir = None
+                else:
+                    return False, subdir
+        return True, subdir
+
 
 
 def is_prefix(p, s):
